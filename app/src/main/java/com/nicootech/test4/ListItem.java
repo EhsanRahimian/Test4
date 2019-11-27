cc74fd718238c726e68db02d9caabb841c2143d1
Ehsan <ehrath1@gmail.com>
2019-11-27 11:52:26 -0800

    good

---
+++ app/src/main/java/com/nicootech/test4/ListItem.java
@@ -0,0 +1,25 @@
+package com.nicootech.test4;
+
+public class ListItem {
+    private String head;
+    private String desc;
+    private String imageUrl;
+
+    public ListItem(String head, String desc) {
+        this.head = head;
+        this.desc = desc;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
---
+++ app/src/main/java/com/nicootech/test4/MyAdapter.java
@@ -0,0 +1,56 @@
+package com.nicootech.test4;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private List<ListItem> listItems;
+    private Context context;
+
+    public MyAdapter(List<ListItem> listItems, Context context) {
+        this.listItems = listItems;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ListItem listItem = listItems.get(position);
+
+        holder.text_view_head.setText(listItem.getHead());
+        holder.text_view_desc.setText(listItem.getDesc());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView text_view_head;
+        public TextView text_view_desc;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            text_view_head = itemView.findViewById(R.id.text_view_head);
+            text_view_desc = itemView.findViewById(R.id.text_view_desc);
+        }
+    }
+}
